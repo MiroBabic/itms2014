f6bccad737652427e0e26301b3e319777a0003da
Miroslav Babic <babicm@agnostic-intelligence.com>
2022-03-26 21:23:59 +0100

    removed useless stuff

--- itms_urls.py
+++
@@ -1,7 +0,0 @@
-base_itms_url = "https://opendata.itms2014.sk"
-
-vyzvy_planovane = "/v2/vyzvy/planovane"
-vyzvy_vyhlasene = "/v2/vyzvy/vyhlasene"
-
-projekty_ukoncene = "/v2/projekty/ukoncene"
-projekty_vrealizacii = "/v2/projekty/vrealizacii"
